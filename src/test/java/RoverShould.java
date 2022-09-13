01e1785726739b8568694dd40db6d923728c1c8d
Tim Turvey <tturvey@hotmail.co.uk>
2022-09-13 11:10:21 +0100

    Red: turns right twice fails

--- src/test/java/RoverShould.java
+++ src/test/java/RoverShould.java
@@ -6,8 +6,14 @@ class RoverShould {
 
     @Test
     void
-    turn_right() {
+    turn_right_once() {
         assertEquals("0:0:E", new Rover().execute("R"));
+    }
+
+    @Test
+    void
+    turn_right_twice() {
+        assertEquals("0:0:S", new Rover().execute("RR"));
     }
 
 }
